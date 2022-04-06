bff0843e05a4b0935ca56078df2644d212efc985
GGG <kuranberitorappu@gmail.com>
2022-04-06 05:55:12 +0000

    emailにユニーク制約追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  before_validation { email.downcase! }
 end
