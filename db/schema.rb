a12d2ba553d364dc18fa049e26422eccc16c7d87
GGG <kuranberitorappu@gmail.com>
2022-04-06 05:33:00 +0000

    emailカラムにインデックス追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_024042) do
+ActiveRecord::Schema.define(version: 2022_04_06_052359) do
 
   create_table "pictures", force: :cascade do |t|
     t.text "image"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_024042) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
