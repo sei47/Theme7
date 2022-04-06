86400ea0b590f26b287f8f22e68dcd0e3a9e3bb1
GGG <kuranberitorappu@gmail.com>
2022-04-06 02:48:31 +0000

    Userテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_092313) do
+ActiveRecord::Schema.define(version: 2022_04_06_024042) do
 
   create_table "pictures", force: :cascade do |t|
     t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
